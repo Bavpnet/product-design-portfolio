dffade734121590d9b085a3dd001e2dcc6b6cdcc
Alexandra Chupkova <a.chupkova@innopolis.university>
2025-08-25 02:02:17 +0300

    add fix scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import ProjectPage from './pages/ProjectPage';
+import ScrollToTop from './components/ScrollToTop'; // Import the new ScrollToTop component
 
 function App() {
   return (
     <Router>
+      <ScrollToTop /> {/* Add ScrollToTop component here */}
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/projects/:projectId" element={<ProjectPage />} />
